6ef5f13039a30a9aca810dbce646772c57bdb364
dengol04 <dengol04@mail.ru>
2025-09-24 11:44:51 +0300

    fixing .csproj file

--- ComputerGraphicsLab2/Program.cs
+++ ComputerGraphicsLab2/Program.cs
@@ -16,7 +16,7 @@ namespace ComputerGraphicsLab2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Task1());
+            Application.Run(new Form1());
         }
     }
 }
