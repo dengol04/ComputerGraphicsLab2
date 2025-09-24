b7a7ceeceb85e970976ccb21405100c8045170e8
dengol04 <dengol04@mail.ru>
2025-09-24 12:09:14 +0300

    renaming pictures for task 1

--- ComputerGraphicsLab2Task1/src/main/java/ru/golubov/computergraphicslab2task1/Lab2Task1.java
+++ ComputerGraphicsLab2Task1/src/main/java/ru/golubov/computergraphicslab2task1/Lab2Task1.java
@@ -68,7 +68,7 @@ public class Lab2Task1 extends Application {
 
 
         Scene scene = new Scene(chooserFileButton, 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Task1");
         stage.setScene(scene);
         stage.show();
     }
